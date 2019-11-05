3ecaa18d8fccab4ed41bdb3c1397f30a2ea1f145
LuccaMarinho <48030687+LuccaMarinho@users.noreply.github.com>
2019-11-05 18:16:39 -0300

    Expose Static Method
    Possibilitar testes desse arquivo.

--- byte_chr.c
+++ byte_chr.c
@@ -4,17 +4,22 @@ unsigned int byte_chr(s,n,c)
 char *s;
 register unsigned int n;
 int c;
+
+//extraido para uma função para possibilitar testes
+static char function(int c, char* s)
 {
   register char ch;
-  register char *t;
+  register char* t;
 
   ch = c;
   t = s;
-  for (;;) {
-    if (!n) break; if (*t == ch) break; ++t; --n;
-    if (!n) break; if (*t == ch) break; ++t; --n;
-    if (!n) break; if (*t == ch) break; ++t; --n;
-    if (!n) break; if (*t == ch) break; ++t; --n;
+  
+  //transformado o for e sequencias de if's em um while
+  while(n && *t != ch)
+  {
+   ++t; 
+   --n;
   }
+  
   return t - s;
 }
