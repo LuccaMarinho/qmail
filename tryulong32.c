61a1910fa75ca24c3fa44633822001b362eae2e7
LuccaMarinho <48030687+LuccaMarinho@users.noreply.github.com>
2019-11-05 17:51:17 -0300

    Adapt Parameter
    O código original possuia uma sequência de somas no main, foi alterado para possibilitar testes

--- tryulong32.c
+++ tryulong32.c
@@ -1,6 +1,8 @@
+// criei uma função, ao invés de fazer a soma no main
 unsigned long addValue(unsigned long u)
 {
   unsigned long j = 0;
+  // transformei a sequência original de somas em um loop
   for(int i = 0, i < u; i++)
     j += 1;
   return j;
